61a464fb9cd4a396f024568360c47a3b02bd741f
Richa Vala <richavala@gmail.com>
2020-10-18 08:34:29 -0500

    Summary added

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ function(input, output) {
     
     
     g <- ggplot(data = tr_orders,aes(x=as.factor(order_dow)))
-    g+geom_bar(color="darkolivegreen3",fill="white")+labs(title="When people order most?",
+    g+geom_bar(color="darkolivegreen3",fill="white")+labs(title="What day of week people order most?",
                                             x="Day of Week",
                                             y="count")
     
@@ -17,9 +17,19 @@ function(input, output) {
       filter(parts_of_day == input$partofday) %>%
       ggplot(.,aes(x=order_hour_of_day)) + 
       geom_bar(color = "darkolivegreen3",fill="white") + 
-      labs(title= "Which day people order most?",
-           x="Hour of week",
+      labs(title= "What hour of day people order most?",
+           x="Hour of the day",
            y="count")
+  })
+  
+  output$stpart = renderPlotly({
+      
+      g <- tr_orders %>% ggplot(.,aes(x=as.factor(order_dow)))
+      g+geom_bar(aes(fill=parts_of_day),position = "dodge")+
+        scale_fill_brewer(palette="Spectral")+labs(title= "What hour of day people order most?",
+                                                   x="Day of Week",
+                                                   y="count")
+     
   })
   
   output$orderagain = renderPlotly({
@@ -35,34 +45,118 @@ function(input, output) {
     
     df <- order_products_train %>% 
       group_by(reordered) %>% 
-      summarise(n_reorder = n()) %>%
-      mutate(ratio = n_reorder/sum(n_reorder))
+      summarise(n_reorder = n())
     
-    g <- ggplot(df, aes(x=reordered,y=n_reorder))
+    g <- ggplot(df, aes(x=as.factor(reordered),y=n_reorder))
     g+geom_col(color="darkolivegreen3",fill="white")+labs(title="Repeat Items In Users Cart",
                                             x="reordered",
                                             y="count")
   })
   
-  df <- order_products_train %>% 
+  test <- order_products_train %>% 
     group_by(reordered) %>% 
     summarise(n_reorder = n()) %>%
     mutate(ratio = n_reorder/sum(n_reorder))
   
   output$repeatprod2 = renderPlotly({
-  g <- ggplot(df, aes(x=reordered,y=ratio))
+  g <- ggplot(test, aes(x=as.factor(reordered,y=ratio)))
   g+geom_col(color="darkolivegreen3",fill="white")+labs(title="Repeat Items In Users Cart",
                                           x="reordered",
                                           y="proportion")
   
   })
   
-  output$table = DT::renderDataTable({
-    
-    tr_orders %>% 
-      group_by(.,order_hour_of_day) %>%
-      summarise(hour_count=n()) %>% 
-      datatable()
+  df_num = order_products_train %>% 
+    group_by(order_id) %>% 
+    summarise(cart_items = last(add_to_cart_order))
+  
+  output$numitems = renderPlotly({
+    g <- ggplot(df_num, aes(x=cart_items))
+    g+geom_histogram(binwidth = 4,color="darkolivegreen3",fill="white")+labs(title="How many items per order ",
+                                                                             x="cart_items",
+                                                                             y="count")
+   })
+  
+  df <- order_products_train %>% group_by(.,product_id)%>%summarise(.,n_items = n()) %>%top_n(.,10,n_items)%>%
+    left_join(select(products,product_id,product_name),by="product_id") %>%
+    arrange(desc(n_items))  
+  
+  output$best = renderPlotly({
+  df%>%ggplot(aes(x=reorder(product_name,-n_items),y=n_items))+
+    geom_bar(stat="identity",color="darkolivegreen3",fill="white") + 
+    theme(axis.text.x=element_text(angle=90, hjust=1),axis.title.x = element_blank())
+  
   })
+  
+  output$da1 = renderPlotly({ 
+    all_csvs <- tr_orderproducts_prior %>% 
+      left_join(tr_orders, by = "order_id") %>% 
+      left_join(products, by = "product_id") %>% 
+      left_join(department, by = "department_id") %>%
+      left_join(aisles,by = "aisle_id") 
+    
+    all_csvs %>%
+      group_by(department, aisle) %>% 
+      count() %>% 
+      ungroup() %>%
+      arrange(desc(n)) %>% 
+      head(10) %>% 
+      ggplot(aes(reorder(aisle, n), y = n, 
+                 fill = reorder(department, desc(n)))) +
+      geom_bar(stat = "identity", col = "black") +
+      labs(x = NULL, y = "count", fill = "Department",
+           caption = "Aisles") +scale_fill_manual(values=c("bisque","aliceblue", "darkseagreen1", "azure3","darkgoldenrod2"))+
+      theme(axis.text.x=element_text(angle=90, hjust=1),
+            axis.title.x = element_blank())
+  })
+  
+ output$da = renderPlotly({ 
+   all_csvs <- tr_orderproducts_prior %>% 
+     left_join(tr_orders, by = "order_id") %>% 
+     left_join(products, by = "product_id") %>% 
+     left_join(department, by = "department_id") %>%
+     left_join(aisles,by = "aisle_id") 
+   
+  all_csvs %>% filter(department == input$department) %>%
+    group_by(department, aisle) %>% 
+    count() %>% 
+    ungroup() %>%
+    arrange(desc(n)) %>% 
+    head(10) %>% 
+    ggplot(aes(reorder(aisle, n), y = n, 
+               fill = reorder(department, desc(n)))) +
+    geom_bar(stat = "identity", col = "black") +
+    labs(x = NULL, y = "count", fill = "Department",
+         caption = "Aisles") +scale_fill_manual(values=c("bisque","aliceblue", "darkseagreen1", "azure3","darkgoldenrod2"))+
+    theme(axis.text.x=element_text(angle=90, hjust=1),
+          axis.title.x = element_blank())
+ })
+  
+  
+  output$order1 = DT::renderDataTable({
+    
+    head(tr_orders)
+      })
+  
+  output$product1 = DT::renderDataTable({
+    
+    head(products)
+  })
+    
+  output$aisles1 = DT::renderDataTable({
+    
+    head(aisles)
+  })
+  
+  output$department1 = DT::renderDataTable({
+    
+    head(department)
+  })
+  
+  output$train1 = DT::renderDataTable({
+    
+    head(order_products_train)
+  })
+  
   #close tab for shinyServer                       
 }
--- ui.R
+++ ui.R
@@ -9,8 +9,8 @@ shinyUI(
     ),
     #sidebar panel 
     dashboardSidebar(
-      sidebarUserPanel("Richa Vala",
-                       image = "https://content.fortune.com/wp-content/uploads/2015/01/instacart-web1.jpg?resize=1200,600"),
+      sidebarUserPanel("Richa Vala"
+                       ),
       sidebarMenu(
         menuItem("Introduction", tabName = "intro", icon = icon("dove")),
         menuItem("Exploration/Analysis", tabName = "explore", icon = icon("map")),
@@ -28,17 +28,21 @@ shinyUI(
           tabName = 'intro',
           fluidPage(
             box(
-              tags$div(h1("Goal : To briefly explore the dataset, highlight the patternsand why it is important to Instacart")),
+              #image = "https://content.fortune.com/wp-content/uploads/2015/01/instacart-web1.jpg?resize=1200,600"
+              tags$div(h2("Goal : To briefly explore the dataset, highlight the patterns and why it is important to Instacart")),
+              br(),
               tags$p(
                 HTML('<p> Market Basket Analysis is one of the key techniques used by large retailers to uncover associations between items.
                      Instacart, a grocery ordering and delivery app, allows users to place grocery order through their website or app which are then fulfilled
                      by a personal shopper.In 2017 Instacart open-sourced 3 million grocery orders. This anonymized dataset contains a sample of over 3 million grocery
-                     orders from more than 200,00 Instacart users. Instacart’s data science team plays a big part in providing this delightful shopping experience. 
+                     orders from more than 200,00 Instacart users.
+                     Instacart’s data science team plays a big part in providing this delightful shopping experience. 
                      Currently they use transactional data to develop models that predict which products a user will buy again, try for the first time, or add to their 
                      cart next during a session.The users are anonymized. There’s no demographics data — no gender, age. Instacart 
                      explained on its blog post that it’s too hard to protect privacy of users if such data is included. In real life, Instacart also
-                     does not collect such data, but does use code scripts to analyze and infer gender from usernames.</p>'),
-                
+                     does not collect such data, but does use code scripts to analyze and infer gender from usernames.<p>'),
+                br(),
+                br(),
                 tags$p("reference: https://tech.instacart.com/3-million-instacart-orders-open-sourced-d40d29ead6f2"),
                 tags$p("reference: https://www.kaggle.com/c/instacart-market-basket-analysis")
                 
@@ -50,7 +54,7 @@ shinyUI(
         tabItem(tabName = "explore",
                   tabsetPanel(
                     tabPanel(
-                      "When people order most?",
+                      "What day people order most?",
                       fluidRow(
                         column(8, plotlyOutput("popday", width = "600px", height = "600px"))
                       ),
@@ -61,16 +65,18 @@ shinyUI(
                     ),  
                     
                     tabPanel(
-                      "What day people order most?",
+                      "Does order volume change throughout the day?",
                       fluidRow(
-                        column(8, plotlyOutput("pophour",width = "600px",height = "600px"))
+                        column(6, plotlyOutput("pophour",width = "600px",height = "600px")),
+                        column(4, plotlyOutput("stpart",width = "600px",height = "600px"))
                       ),
                       fluidRow(
                         selectizeInput('partofday', 'Please select time of day:', c("Morning","Afternoon", "Evening"))
                       ),
                       fluidRow(
                         h2("Summary"),
-                        "To be filled"
+                        "People do more shopping on weekend (assuming 1 for Saturday and 0 for Synday). The 
+                        volume of order increases sharply from early morning till 11 am."
                       )
                     ),
                    
@@ -81,12 +87,12 @@ shinyUI(
                       ),
                       fluidRow(
                         h2("Summary"),
-                        "To be filled"
+                        "Generally, the users place their order at day 5 or at day 30"
                       )
                     ),
                    
                    tabPanel(
-                     "What is the proportion of the repeat products users prefer?",
+                     "What is the count/poportion of the repeat products?",
                      fluidRow(
                        column(6, plotlyOutput("repeatprod1",width = "600px",height = "600px")),
                        column(4, plotlyOutput("repeatprod2",width = "600px",height = "600px"))
@@ -95,9 +101,51 @@ shinyUI(
                        h2("Summary"),
                        "To be filled"
                      )
-                   )
-              )                 
+                   ),
+                   
+                  tabPanel(
+                     "How large are orders?",
+                     fluidRow(
+                       column(6, plotlyOutput("numitems",width = "600px",height = "600px"))
+                       ),
+                     fluidRow(
+                       h2("Summary"),
+                       "To be filled"
+                     )
+                   ),
+              
+                  tabPanel(
+                    " What are best selling products?",
+                    fluidRow(
+                      column(6, plotlyOutput("best",width = "600px",height = "600px"))
+                      ),
+                    fluidRow(
+                      h2("Summary"),
+                      "Their first top 10 selling items are fresh produce, especially those 
+                       which are organic."
+                    )
+                  ),
+                  
+                  tabPanel(
+                    "What are the top aisles/departments?",
+                    fluidRow(
+                      column(6, plotlyOutput("da1",width = "600px",height = "600px")),
+                      column(6, plotlyOutput("da",width = "600px",height = "600px"))
+                    ),
+                    fluidRow(
+                      selectizeInput('department', 'Please select department to filter:', 
+                                     c("produce","dairy eggs", "snacks","beverages","bakery"),
+                                     selected=NULL)
+                     ),
+                    fluidRow(
+                      h2("Summary"),
+                      "The fresh fruits and vegetables from the produce department are more popular."
+                    )
+                  )
+                  
+          )
         ),
+        
         #Conclusion####
         tabItem(tabName = "conc",
                 fluidRow(box(
@@ -111,13 +159,44 @@ shinyUI(
                   width = 12)
                 )
         ),
-        #dataset####
+        #data####
         tabItem(tabName = "data",
-                fluidRow(
-                    DT::dataTableOutput("table")
-                )
-                # tags$p("https://www.kaggle.com/c/instacart-market-basket-analysis/data")
-        ),
+                tabsetPanel(
+                  tabPanel(
+                    "orders",
+                    fluidRow(
+                      DT::dataTableOutput('order1')
+                    )
+                  ), 
+                 tabPanel(
+                  "products",
+                  fluidRow(
+                      DT::dataTableOutput('product1')
+                      )
+                 ), 
+                  tabPanel(
+                    "aisles",
+                    fluidRow(
+                      DT::dataTableOutput('aisles1')
+                    )
+                  ),      
+                  tabPanel(
+                    "departments",
+                    fluidRow(
+                      DT::dataTableOutput('department1')
+                        )
+                  ),     
+                  tabPanel(
+                    "order_products",
+                    fluidRow(
+                      DT::dataTableOutput('train1')
+                        )
+                      )
+                     
+                  )
+                ),
+         
+        
         #self-intro####
         tabItem(tabName = "self",
                 fluidRow(
@@ -133,8 +212,13 @@ shinyUI(
                     tags$a(href = 'https://www.linkedin.com/in/richavala/'),
                     width = 6,
                     height = 600)))
-        )
-      )
+             )
+        
+      
+      
+     )
     )
+    
+    
   )
-)
+ )
