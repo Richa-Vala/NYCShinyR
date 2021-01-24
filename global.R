a573bec6e1b998e42b61d417f48246255afd9af2
Richa Vala <60301209+Richa-Vala@users.noreply.github.com>
2021-01-24 15:10:43 -0600

    Update global.R

--- global.R
+++ global.R
@@ -1,3 +1,6 @@
+# The global.R file run once to load in your data before the app starts.
+
+# Load libraries
 library(knitr)
 library(tidyr)
 library(tidyverse)
@@ -8,19 +11,12 @@ library(tidyverse)
 library(plotly)
 library(shinydashboard)
 library(DT)
-      
-#orders <- readr::read_csv("archive/tr_order_products__prior.csv")
+ 
+# Load data files
 products <- readr::read_csv("archive/products.csv")
 aisles <- readr::read_csv("archive/aisles.csv")
 department <- readr::read_csv("archive/departments.csv")
-#ordprod <- readr::read_csv("archive/order_products__train.csv")
-
 tr_orders <- readr::read_csv("archive/tr_orders.csv")
 tr_orderproducts_prior <- readr::read_csv("archive/tr_order_products__prior.csv")
-
 order_products_train <- readr::read_csv("archive/order_products__train.csv")
 
-
-# products <- readr::read_csv("archive/products.csv")
-# aisles <- readr::read_csv("aisles.csv")
-# department <- readr::read_csv("departments.csv")
